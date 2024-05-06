186a1c1751826d4ad6b4133d1860a08ba60d202e
JojoFR1 <JojoFR1@users.noreply.github.com>
2024-05-06 20:49:00 +0200

    Movements Feet - Part 1.4 (More fixes + finished naming French)

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
 @echo off
 
-rm AlzheimouilleDataPack.zip 
-rm AlzheimouilleResourcePack.zip 
-zip -r AlzheimouilleDataPack.zip . -x 'AlzheimouilleResourcePack.zip' 'build.bat' 'rename.ps1' '.gitignore' '.git/*' 'assets/*'
-zip -r AlzheimouilleResourcePack.zip . -x 'AlzheimouilleDataPack.zip' 'build.bat' 'rename.ps1' '.gitignore' '.git/*' 'data/*'
+set VER="1.5.0-beta.2"
+
+rm "AlzheimouilleDataPack_%VER%.zip" 
+rm "AlzheimouilleResourcePack_%VER%.zip" 
+zip -r "AlzheimouilleDataPack_%VER%.zip" . -x 'build.bat' 'rename.ps1' '.gitignore' '.git/*' 'assets/*'
+zip -r "AlzheimouilleResourcePack_%VER%.zip" . -x "AlzheimouilleDataPack_%VER%.zip" 'build.bat' 'rename.ps1' '.gitignore' '.git/*' 'data/*'
