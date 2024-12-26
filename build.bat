7e0333c79b363bcf744028c6d6afbcb54b346463
JojoFR1 <JojoFR1@users.noreply.github.com>
2024-12-26 19:12:52 +0100

    Fixed build

--- build.bat
+++ build.bat
@@ -2,7 +2,8 @@
 
 set VER="1.6.0"
 
-if not exist "build" mkdir "build"
+if exist "build" rmdir /S /Q "build"
+mkdir "build"
 
 zip -r "build/AlzheimouilleDataPack_%VER%.zip" . -x 'build.bat' 'spyglass.json' 'rename.ps1' '.gitignore' '.git/*' 'assets/*' 'build/*
 zip -r "build/AlzheimouilleResourcePack_%VER%.zip" . -x "AlzheimouilleDataPack_%VER%.zip" 'build.bat' 'spyglass.json'  'rename.ps1' '.gitignore' '.git/*' 'data/*' 'build/*'
